7ccf422c10574b5f251ccd6ad6e28e9bc302da32
Wionek <krystianjonca43@gmail.com>
2022-04-26 18:15:59 +0200

    Fixed warnings

--- components/Navigation/Logo.tsx
+++ components/Navigation/Logo.tsx
@@ -10,23 +10,25 @@ const Logo: React.FC<LogoProps> = ({ navLogo = true }) => {
   if (!navLogo)
     return (
       <Link href="/" passHref>
-        <Image src="/Logo.webp" alt="Toss Me A Coins" width="240" height="64" />
+        <span>
+          <Image src="/Logo.webp" alt="Toss Me A Coins" width="240" height="64" />
+        </span>
       </Link>
     );
 
   return (
     <>
-      <div className="hidden sm:justify-center sm:items-center sm:flex h-16 w-auto cursor-pointer">
-        <Link href="/" passHref>
-          <Image src="/Logo.webp" alt="Toss Me A Coins" width="240" height="64" />
-        </Link>
-      </div>
+      <Link href="/" passHref>
+        <span>
+          <div className="hidden sm:justify-center sm:items-center sm:flex h-16 w-auto cursor-pointer">
+            <Image src="/Logo.webp" alt="Toss Me A Coins" width="240" height="64" />
+          </div>
 
-      <div className="flex justify-center items-center sm:hidden h-16 w-auto cursor-pointer">
-        <Link href="/" passHref>
-          <Image src="/LogoSmall.webp" alt="Toss Me A Coins" width="64" height="64" />
-        </Link>
-      </div>
+          <div className="flex justify-center items-center sm:hidden h-16 w-auto cursor-pointer">
+            <Image src="/LogoSmall.webp" alt="Toss Me A Coins" width="64" height="64" />
+          </div>
+        </span>
+      </Link>
     </>
   );
 };
