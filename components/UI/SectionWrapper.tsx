17c1c8274a181cc07235fdd42047c5d95f35521d
Wionek <krystianjonca43@gmail.com>
2022-04-26 18:16:17 +0200

    Fixed warnings

--- components/UI/SectionWrapper.tsx
+++ components/UI/SectionWrapper.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
 interface SectionWrapperProps {
-  id?: String;
+  id?: string;
   readonly children: React.ReactNode;
 }
 
 const SectionWrapper: React.FC<SectionWrapperProps> = ({ id, children }) => {
   return (
-    <section
-      className="flex items-center justify-center p-2 md:p-4"
-      {...(typeof id !== undefined && `id=${id}`)}
-    >
+    <section id={id} className="flex items-center justify-center p-2 md:p-4">
       {children}
     </section>
   );
