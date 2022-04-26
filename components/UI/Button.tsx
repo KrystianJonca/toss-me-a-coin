0a1648d2b142b1c8d60a81f2481cdf19123c21c1
Wionek <krystianjonca43@gmail.com>
2022-04-26 15:25:04 +0200

    Added button

---
+++ components/UI/Button.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface ButtonProps {
+  onClick: () => void;
+  readonly children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+  return (
+    <button
+      className="text-sm sm:text-md md:text-lg bg-lightBlue hover:bg-darkBlue transition-colors text-white font-bold py-2 px-4 rounded-full"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
