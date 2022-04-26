f8e30f5930020755957936f81ca84499ee66b38a
Wionek <krystianjonca43@gmail.com>
2022-04-26 18:37:33 +0200

    Fixed an bug

--- components/UI/GradientHeading.tsx
+++ components/UI/GradientHeading.tsx
@@ -7,9 +7,7 @@ interface GradientHeadingProps {
 const GradientHeading: React.FC<GradientHeadingProps> = ({ children }) => {
   return (
     <span className="block bg-clip-text text-transparent bg-gradient-to-r to-darkBlue from-gold p-2">
-      <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl p-2">
-        Crypto Donations Made Easy For Everyone.
-      </h1>
+      <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl p-2">{children}</h1>
     </span>
   );
 };
