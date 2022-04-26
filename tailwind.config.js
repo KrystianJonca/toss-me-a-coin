bb72dbf3cdcf4e158da66ed6b2614f5abfe02c2a
Wionek <krystianjonca43@gmail.com>
2022-04-26 17:24:19 +0200

    Updated exports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '380px',
+      },
+      colors: {
+        lightBlue: '#6666ff',
+        darkBlue: '#5e17eb',
+        gold: '#c9e265',
+      },
+    },
   },
   plugins: [],
 };
