9243a055dfe290e6f3fbd7aaf064c6b920c5f3b6
Wionek <krystianjonca43@gmail.com>
2022-05-04 20:14:55 +0200

    Added creator context

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Image from 'next/image';
+import React, { useContext } from 'react';
 import Link from 'next/link';
-
 import Button from '../UI/Button';
 import MenuLink from './MenuLink';
 import Logo from './Logo';
 
+import { creatorContext } from '../../context/creatorContext';
+
 const Navbar: React.FC = () => {
+  const { connectWallet, creator } = useContext(creatorContext);
+
   return (
     <nav className="fixed h-16 top-0 bg-white shadow-sm inset-x-0 z-50">
       <div className="flex h-full items-center justify-between max-w-6xl mx-auto px-1 sm:px-4">
@@ -24,7 +26,23 @@ const Navbar: React.FC = () => {
           <MenuLink href="/about" className="mr-1 sm:mr-2">
             About
           </MenuLink>
-          <Button onClick={() => console.log('BTN Clicked')}>Get started</Button>
+          {typeof creator == undefined ? (
+            <Button onClick={connectWallet}>Connect Wallet</Button>
+          ) : (
+            !creator.username &&
+            creator.ethAddress && (
+              <Link href="/create" passHref>
+                <Button onClick={() => console.log('redirecting...')}>Create Page</Button>
+              </Link>
+            )
+          )}
+          {creator.username && (
+            <Link href={`/creators/${creator.username}`} passHref>
+              <span className="text-sm sm:text-md md:text-lg bg-[#6666ff] hover:bg-[#5e17eb] text-white font-bold py-2 px-4 rounded-full cursor-pointer">
+                {creator.username}
+              </span>
+            </Link>
+          )}
         </div>
       </div>
     </nav>
